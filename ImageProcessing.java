a16f7c098901808d6b3d9283467ec6512c8cdc9c
Andi <28365760+And1B@users.noreply.github.com>
2022-12-23 11:16:05 +0100

    Formatting

--- ImageProcessing.java
+++ ImageProcessing.java
@@ -9,49 +9,49 @@ import javax.imageio.ImageIO;
 
 public class ImageProcessing {
 	public static void main(String[] args) {    
-    // Get URL from user:
-    Scanner sc = new Scanner(System.in)
-    System.out.print("Enter the url: ");  
-    String url = sc.nextLine();    
-      
-    // loads image using URL!
-		int[][] imageData = imgToTwoD(url);
+	    // Get URL from user:
+	    Scanner sc = new Scanner(System.in)
+	    System.out.print("Enter the url: ");  
+	    String url = sc.nextLine();    
 
-		// viewImageData(imageData);
-    
-		int[][] trimmed = trimBorders(imageData, 60);
-		twoDToImage(trimmed, "./trimmed_image.jpg");
+	    // loads image using URL!
+	    int[][] imageData = imgToTwoD(url);
 
-    int[][] negative = negativeColor(imageData);
-		twoDToImage(negative, "./negative_image.jpg");
+	    // viewImageData(imageData);
 
-    int[][] horizontallyStretched = stretchHorizontally(imageData);
-    twoDToImage(horizontallyStretched, "./horizontally_stretched_image.jpg");
+	    int[][] trimmed = trimBorders(imageData, 60);
+	    twoDToImage(trimmed, "./trimmed_image.jpg");
 
-    int[][] verticallyShrunk = shrinkVertically(imageData);
-    twoDToImage(verticallyShrunk, "./vertically_shrunk_image.jpg");
+	    int[][] negative = negativeColor(imageData);
+	    twoDToImage(negative, "./negative_image.jpg");
 
-    int[][] invertedImage = invertImage(imageData);
-    twoDToImage(invertedImage, "./inverted_image.jpg");
+	    int[][] horizontallyStretched = stretchHorizontally(imageData);
+	    twoDToImage(horizontallyStretched, "./horizontally_stretched_image.jpg");
 
-    int[][] colorFilteredImage = colorFilter(imageData, -75, 30, -30);
-    twoDToImage(colorFilteredImage, "./color_filtered_image.jpg");
-		int[][] allFilters = stretchHorizontally(shrinkVertically(colorFilter(negativeColor(trimBorders(invertImage(imageData), 50)), 200, 20, 40)));
-    twoDToImage(allFilters, "./all_filtered_image.jpg");
+	    int[][] verticallyShrunk = shrinkVertically(imageData);
+	    twoDToImage(verticallyShrunk, "./vertically_shrunk_image.jpg");
 
-		// Painting with pixels
-    int[][] blankCanvas = new int[500][500];
-    int[][] randomImage = paintRandomImage(blankCanvas);
-    twoDToImage(randomImage, "./random_image.jpg");
+	    int[][] invertedImage = invertImage(imageData);
+	    twoDToImage(invertedImage, "./inverted_image.jpg");
 
-    int[][] anotherBlankCanvas = new int[500][500];
-    int[] rgba = {0, 0, 123, 255};
-    int[][] rectangleImage = paintRectangle(anotherBlankCanvas, 100, 50, 15, 40, getColorIntValFromRGBA(rgba));
-    twoDToImage(rectangleImage, "./rectangle.jpg");
+	    int[][] colorFilteredImage = colorFilter(imageData, -75, 30, -30);
+	    twoDToImage(colorFilteredImage, "./color_filtered_image.jpg");
+	    int[][] allFilters = stretchHorizontally(shrinkVertically(colorFilter(negativeColor(trimBorders(invertImage(imageData), 50)), 200, 20, 40)));
+	    twoDToImage(allFilters, "./all_filtered_image.jpg");
 
-    int[][] newCanvas = new int[500][500];
-    int[][] randomRectanglesCanvas = generateRectangles(newCanvas, 1000);
-    twoDToImage(randomRectanglesCanvas, "./randomRectanglesCanvas.jpg");
+	    // Painting with pixels
+	    int[][] blankCanvas = new int[500][500];
+	    int[][] randomImage = paintRandomImage(blankCanvas);
+	    twoDToImage(randomImage, "./random_image.jpg");
+
+	    int[][] anotherBlankCanvas = new int[500][500];
+	    int[] rgba = {0, 0, 123, 255};
+	    int[][] rectangleImage = paintRectangle(anotherBlankCanvas, 100, 50, 15, 40, getColorIntValFromRGBA(rgba));
+	    twoDToImage(rectangleImage, "./rectangle.jpg");
+
+	    int[][] newCanvas = new int[500][500];
+	    int[][] randomRectanglesCanvas = generateRectangles(newCanvas, 1000);
+	    twoDToImage(randomRectanglesCanvas, "./randomRectanglesCanvas.jpg");
 	}
 
 	// Image Processing Methods
@@ -70,114 +70,120 @@ public class ImageProcessing {
 			return imageTwoD;
 		}
 	}
+	
 	public static int[][] negativeColor(int[][] imageTwoD) {
-		// TODO: Fill in the code for this method
-    int[][] negativeImg = new int[imageTwoD.length][imageTwoD[0].length];
-    for (int i = 0; i < imageTwoD.length; i++) {
-      for (int j = 0; j < imageTwoD[i].length; j++) {
-        int[] rgba = getRGBAFromPixel(imageTwoD[i][j]);
-        rgba[0] = 255 - rgba[0];
-        rgba[1] = 255 - rgba[1];
-        rgba[2] = 255 - rgba[2];
-        negativeImg[i][j] = getColorIntValFromRGBA(rgba);
-      }
-    }
-    return negativeImg;
-  }
+	    int[][] negativeImg = new int[imageTwoD.length][imageTwoD[0].length];
+	    for (int i = 0; i < imageTwoD.length; i++) {
+	    	for (int j = 0; j < imageTwoD[i].length; j++) {
+			int[] rgba = getRGBAFromPixel(imageTwoD[i][j]);
+			rgba[0] = 255 - rgba[0];
+			rgba[1] = 255 - rgba[1];
+			rgba[2] = 255 - rgba[2];
+			negativeImg[i][j] = getColorIntValFromRGBA(rgba);
+	    	}
+	    }
+	    return negativeImg;
+	  }
+	
 	public static int[][] stretchHorizontally(int[][] imageTwoD) {
 		int[][] horizontallyStretchedImg = new int[imageTwoD.length][imageTwoD[0].length*2];
-    int modifiedColumnIndex = 0;
-    for(int rowIndex=0; rowIndex<imageTwoD.length; rowIndex++){
-      for(int columnIndex=0; columnIndex<imageTwoD[rowIndex].length;columnIndex++){
-        modifiedColumnIndex = columnIndex*2;
-        horizontallyStretchedImg[rowIndex][modifiedColumnIndex] = imageTwoD[rowIndex][columnIndex];
-        horizontallyStretchedImg[rowIndex][modifiedColumnIndex + 1] = imageTwoD[rowIndex][columnIndex];
-      }
-    }
-    return horizontallyStretchedImg;
+    		int modifiedColumnIndex = 0;
+		for(int rowIndex=0; rowIndex<imageTwoD.length; rowIndex++){
+			for(int columnIndex=0; columnIndex<imageTwoD[rowIndex].length;columnIndex++){
+				modifiedColumnIndex = columnIndex*2;
+				horizontallyStretchedImg[rowIndex][modifiedColumnIndex] = imageTwoD[rowIndex][columnIndex];
+				horizontallyStretchedImg[rowIndex][modifiedColumnIndex + 1] = imageTwoD[rowIndex][columnIndex];
+      			}
+    		}
+    		return horizontallyStretchedImg;
 	}
+	
 	public static int[][] shrinkVertically(int[][] imageTwoD) {
 		int[][] verticallyShrunkImg = new int[imageTwoD.length / 2][imageTwoD[0].length];
-    for(int columnIndex=0; columnIndex<imageTwoD[0].length; columnIndex++){
-      for(int rowIndex=0; rowIndex<imageTwoD.length-1; rowIndex+=2){
-        verticallyShrunkImg[rowIndex/2][columnIndex] = imageTwoD[rowIndex][columnIndex];
-      }
-    }
+    		for(int columnIndex=0; columnIndex<imageTwoD[0].length; columnIndex++){
+      			for(int rowIndex=0; rowIndex<imageTwoD.length-1; rowIndex+=2){
+        			verticallyShrunkImg[rowIndex/2][columnIndex] = imageTwoD[rowIndex][columnIndex];
+      			}
+    		}
 		return verticallyShrunkImg;
 	}
+	
 	public static int[][] invertImage(int[][] imageTwoD) {
 		int[][] invertedImage = new int[imageTwoD.length][imageTwoD[0].length];
-    for(int columnIndex=0; columnIndex<imageTwoD.length;columnIndex++){
-      for(int rowIndex=0; rowIndex<imageTwoD[columnIndex].length;rowIndex++){
-        invertedImage[columnIndex][rowIndex] = imageTwoD[(imageTwoD.length - 1) - columnIndex][(imageTwoD[columnIndex].length - 1) - rowIndex];
-      }
-    }
+    		for(int columnIndex=0; columnIndex<imageTwoD.length;columnIndex++){
+      			for(int rowIndex=0; rowIndex<imageTwoD[columnIndex].length;rowIndex++){
+        			invertedImage[columnIndex][rowIndex] = imageTwoD[(imageTwoD.length - 1) - columnIndex][(imageTwoD[columnIndex].length - 1) - rowIndex];
+      			}
+    		}
 		return invertedImage;
 	}
+	
 	public static int[][] colorFilter(int[][] imageTwoD, int redChangeValue, int greenChangeValue, int blueChangeValue) {
-    int[][] filteredImage = new int[imageTwoD.length][imageTwoD[0].length];
-    for(int rowIndex=0; rowIndex<imageTwoD.length; rowIndex++){
-      for(int columnIndex=0; columnIndex<imageTwoD[rowIndex].length; columnIndex++){
-        int[] rgba_values = getRGBAFromPixel(imageTwoD[rowIndex][columnIndex]);
-        rgba_values[0] += redChangeValue;
-        if(rgba_values[0] < 0){
-          rgba_values[0] = 0;
-        } else if (rgba_values[0] > 255){
-          rgba_values[0] = 255;
-        }
-        rgba_values[1] += greenChangeValue;
-        if(rgba_values[1] < 0){
-          rgba_values[1] = 0;
-        } else if (rgba_values[1] > 255){
-          rgba_values[1] = 255;
-        }
-        rgba_values[2] += blueChangeValue;
-        if(rgba_values[2] < 0){
-          rgba_values[2] = 0;
-        } else if (rgba_values[2] > 255){
-          rgba_values[2] = 255;
-        }
-        filteredImage[rowIndex][columnIndex] = getColorIntValFromRGBA(rgba_values);
-      }
-    }
+    		int[][] filteredImage = new int[imageTwoD.length][imageTwoD[0].length];
+    		for(int rowIndex=0; rowIndex<imageTwoD.length; rowIndex++){
+      			for(int columnIndex=0; columnIndex<imageTwoD[rowIndex].length; columnIndex++){
+				int[] rgba_values = getRGBAFromPixel(imageTwoD[rowIndex][columnIndex]);
+				rgba_values[0] += redChangeValue;
+				if(rgba_values[0] < 0){
+				  rgba_values[0] = 0;
+				} else if (rgba_values[0] > 255){
+				  rgba_values[0] = 255;
+				}
+				rgba_values[1] += greenChangeValue;
+				if(rgba_values[1] < 0){
+				  rgba_values[1] = 0;
+				} else if (rgba_values[1] > 255){
+				  rgba_values[1] = 255;
+				}
+				rgba_values[2] += blueChangeValue;
+				if(rgba_values[2] < 0){
+				  rgba_values[2] = 0;
+				} else if (rgba_values[2] > 255){
+				  rgba_values[2] = 255;
+				}
+				filteredImage[rowIndex][columnIndex] = getColorIntValFromRGBA(rgba_values);
+		      	}
+		}
 		return filteredImage;
 	}
 
 	// Painting Methods
 	public static int[][] paintRandomImage(int[][] canvas) {
-    Random rand = new Random();
-    for(int rowIndex=0; rowIndex<canvas.length; rowIndex++){
-      for(int columnIndex=0; columnIndex<canvas[rowIndex].length; columnIndex++){
-        int firstInt = rand.nextInt(256);
-        int secondInt = rand.nextInt(256);
-        int thirdInt = rand.nextInt(256);
-        int[] randomRGBA = { firstInt, secondInt, thirdInt, 255};
-        canvas[rowIndex][columnIndex] = getColorIntValFromRGBA(randomRGBA);
-      }
-    }
+    		Random rand = new Random();
+    		for(int rowIndex=0; rowIndex<canvas.length; rowIndex++){
+      			for(int columnIndex=0; columnIndex<canvas[rowIndex].length; columnIndex++){
+				int firstInt = rand.nextInt(256);
+				int secondInt = rand.nextInt(256);
+				int thirdInt = rand.nextInt(256);
+				int[] randomRGBA = { firstInt, secondInt, thirdInt, 255};
+				canvas[rowIndex][columnIndex] = getColorIntValFromRGBA(randomRGBA);
+      			}
+    		}
 		return canvas;
 	}
+	
 	public static int[][] paintRectangle(int[][] canvas, int width, int height, int rowPosition, int colPosition, int color) {
 		for(int rowIndex=0; rowIndex<canvas.length; rowIndex++){
-      for(int columnIndex=0; columnIndex<canvas[rowIndex].length; columnIndex++){
-        if((rowIndex >= rowPosition && rowIndex <= rowPosition + width) && (columnIndex >= colPosition && columnIndex <= colPosition + height)) {
-          canvas[rowIndex][columnIndex] = color;
-        }
-      }
-    }
+      			for(int columnIndex=0; columnIndex<canvas[rowIndex].length; columnIndex++){
+        			if((rowIndex >= rowPosition && rowIndex <= rowPosition + width) && (columnIndex >= colPosition && columnIndex <= colPosition + height)) {
+          				canvas[rowIndex][columnIndex] = color;
+        			}
+      			}
+    		}
 		return canvas;
 	}
+	
 	public static int[][] generateRectangles(int[][] canvas, int numRectangles) {
 		Random rand = new Random();
-    for(int counter=0; counter < numRectangles; counter++){
-      int randomWidth = rand.nextInt(canvas[0].length);
-      int randomHeight = rand.nextInt(canvas.length);
-      int randomRowPosition = rand.nextInt(canvas.length - randomHeight);
-      int randomColumnPosition = rand.nextInt(canvas[0].length - randomWidth);
-      int[] randomRGBA = { rand.nextInt(256), rand.nextInt(256), rand.nextInt(256), 255};
-      int randomColor = getColorIntValFromRGBA(randomRGBA);
-      canvas = paintRectangle(canvas, randomWidth, randomHeight, randomRowPosition, randomColumnPosition, randomColor);
-    }
+    		for(int counter=0; counter < numRectangles; counter++){
+			int randomWidth = rand.nextInt(canvas[0].length);
+		        int randomHeight = rand.nextInt(canvas.length);
+		        int randomRowPosition = rand.nextInt(canvas.length - randomHeight);
+		        int randomColumnPosition = rand.nextInt(canvas[0].length - randomWidth);
+		        int[] randomRGBA = { rand.nextInt(256), rand.nextInt(256), rand.nextInt(256), 255};
+		        int randomColor = getColorIntValFromRGBA(randomRGBA);
+		        canvas = paintRectangle(canvas, randomWidth, randomHeight, randomRowPosition, randomColumnPosition, randomColor);
+   		}
 		return canvas;
 	}
 
@@ -208,6 +214,7 @@ public class ImageProcessing {
 			return null;
 		}
 	}
+	
 	public static void twoDToImage(int[][] imgData, String fileName) {
 		try {
 			int imgRows = imgData.length;
@@ -224,10 +231,12 @@ public class ImageProcessing {
 			System.out.println("Failed to save image: " + e.getLocalizedMessage());
 		}
 	}
+	
 	public static int[] getRGBAFromPixel(int pixelColorValue) {
 		Color pixelColor = new Color(pixelColorValue);
 		return new int[] { pixelColor.getRed(), pixelColor.getGreen(), pixelColor.getBlue(), pixelColor.getAlpha() };
 	}
+	
 	public static int getColorIntValFromRGBA(int[] colorData) {
 		if (colorData.length == 4) {
 			Color color = new Color(colorData[0], colorData[1], colorData[2], colorData[3]);
@@ -237,6 +246,7 @@ public class ImageProcessing {
 			return -1;
 		}
 	}
+	
 	public static void viewImageData(int[][] imageTwoD) {
 		if (imageTwoD.length > 3 && imageTwoD[0].length > 3) {
 			int[][] rawPixels = new int[3][3];
